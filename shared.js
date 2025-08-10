a822063768d8a2023bc738361dc01141caf4d56f
svetlozara-godzheva <n01754024@humber.ca>
2025-08-10 19:20:54 -0400

    Implemented statistics feature

--- shared.js
+++ shared.js
@@ -29,13 +29,63 @@ export async function delay(interval) {
     return result;
 }
 
-export function collectResults(results) {
+export function collectResults(results, quizTime) {
     let wrongAnswers = results.filter((x) => x.answer != x.translation);
     let rightAnswers = results.filter((x) => x.answer === x.translation);
     localStorage.setItem("wrong-answers", JSON.stringify(wrongAnswers));
+    addQuizTime(quizTime);
+    increaseStatistic("wrong-answers-count", wrongAnswers.length);
+    increaseStatistic("right-answers-count", rightAnswers.length);
+}
+
+function increaseStatistic(key, count) {
+    let localStorageCount = localStorage.getItem(key);
+    if (localStorageCount) {
+        let number = parseInt(localStorageCount);
+        number += count;
+        localStorage.setItem(key, number);
+    } else {
+        localStorage.setItem(key, count);
+    }
+}
+
+export function getStatistic(key) {
+    let localStorageStatistic = localStorage.getItem(key);
+    if (localStorageStatistic) {
+        let statistic = parseInt(localStorageStatistic);
+        return statistic;
+    } else {
+        return 0;
+    }
+}
+
+function addQuizTime(quizTime) {
+    let localStorageTimes = localStorage.getItem("quiz-times");
+    if (localStorageTimes) {
+        let times = JSON.parse(localStorageTimes);
+        times.push(quizTime);
+        localStorage.setItem("quiz-times", JSON.stringify(times));
+    } else {
+        localStorage.setItem("quiz-times", JSON.stringify([quizTime]));
+    }
+}
+
+export function getAverageQuizTime() {
+    let localStorageTimes = localStorage.getItem("quiz-times");
+    if (!localStorageTimes) {
+        return 0;
+    }
+    let times = JSON.parse(localStorageTimes);
+
+    let total = times.reduce((sum, time) => {
+        return sum + time;
+    }, 0);
+    return Math.ceil((total / times.length));
 }
 
 export function getWrongAnswers() {
     let wrongAnswers = localStorage.getItem("wrong-answers");
     return JSON.parse(wrongAnswers);
 }
+
+
